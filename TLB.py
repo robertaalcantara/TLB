3cc7e26f5d76836fe39b00373a4d099ddbf8d3d6
EnergyFall266 <leonardo.vanzin@hotmail.com>
2022-04-07 16:41:41 -0300

    base

---
+++ TLB.py
@@ -0,0 +1,81 @@
+import numpy as py
+
+#lê os arquivos para cada tamanho de TLB e retorna as taxas
+def simulador(tamanho):
+    arquivos = ['bigone', 'bzip', 'gcc', 'sixpack', 'swim']
+    #arquivos = ['bzip']
+    tlb = []
+    #variaveis que retornam todos os valores calculados dai
+    taxasAcerto = []
+    taxasAdd = []
+    taxasSub = []
+    contInTlb = []
+    contNewTlb = []
+    contSubTlb = []
+
+    for arquivo in arquivos:
+
+        inTlb = 0 #ja esta na tlb
+        newTlb = 0 #nao esta na tlb, e acrescenta na tlb
+        subTlb = 0 #substitui um valor ja escrito
+
+        file = open('Arquivos/'+arquivo+'.trace', 'r')
+
+        linhas = file.readlines()
+
+        for linha in linhas:
+            endereco = linha[:5] #pega os 5 primeiros números, referentes ao endereço
+
+            #verifica se o endereço já se encontra na TLB
+            if(endereco in tlb):
+                inTlb += 1
+            #verifica se existe espaço livre na TLB e adiciona o endereço ao final
+            elif(len(tlb) < tamanho):
+                newTlb += 1
+                tlb.append(endereco)
+            #elimina o endereço da primeira posição da TLB para liberar espaço (FIFO)
+            #adiciona o endereço atual ao final da TLB
+            else:
+                subTlb += 1
+                tlb.pop(0)
+                tlb.append(endereco)
+        
+        taxaAcerto = inTlb / len(linhas) 
+        taxasAcerto.append(taxaAcerto)
+        contInTlb.append(inTlb)
+        
+        taxaAdd = newTlb / len(linhas) #taxa de adições à TLB com espaço livre
+        taxasAdd.append(taxaAdd)
+        contNewTlb.append(newTlb)
+        
+        taxaSub = subTlb / len(linhas) #taxa de substituições na TLB
+        taxasSub.append(taxaSub)
+        contSubTlb.append(subTlb)
+
+    file.close()
+    return contInTlb, taxasAcerto, contNewTlb, taxasAdd, contSubTlb, taxasSub
+    
+
+if __name__ == "__main__":
+    tm = 800
+    tTlb = 80
+    tmac = [] #tempo médio de acesso
+
+    for i in range(1,13):
+        
+        tam = pow(2,i)
+        print(tam)
+
+        contInTlb, taxasAcerto, contNewTlb, taxasAdd, contSubTlb, taxasSub = simulador(tam)
+        print(contInTlb, taxasAcerto, contNewTlb, taxasAdd, contSubTlb, taxasSub)
+
+        for i in taxasAcerto:
+            time = i * (tTlb+tm) + (1 - i) * (tTlb + 2 * tm)
+            tmac.append(time)
+
+        print("tamanho: ", tam)
+        print("tmac: ", tmac)
+        # tmac = alfa(Ttlb+Tm)+(1 - alfa)(Ttlb + 2 * Tm),onde:
+        # taxasAcerto[i] = taxa de acerto da tlb
+        # tTlb = tempo de acesso à tlb
+        # tm = tempo médio
---
+++ teste.py
@@ -0,0 +1,20 @@
+# list = [2,3,5,7,4]
+
+
+# list.append(9)
+
+# print(list)
+# list.pop(0)
+# print(list)
+# list.append(8)
+# print(list)
+# list.pop(0)
+# print(list)
+
+
+#file = open('Arquivos/sixpack.trace', 'r')
+#linhas = file.readlines()
+#print(len(linhas))
+
+for i in range(1,13):
+    print(pow(2,i))
